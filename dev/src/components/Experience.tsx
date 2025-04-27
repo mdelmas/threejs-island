f84803d7309485743a70617f0239882f80e686bd
Morgane Delmas <mdelmas93@gmail.com>
2025-04-27 15:00:41 +0200

    [feat] limiting camera control, adding horizontal limit

--- dev/src/components/Experience.tsx
+++ dev/src/components/Experience.tsx
@@ -8,6 +8,7 @@ import Fishes from "./Fishes";
 import PaperMaterial from "./PaperMaterial";
 import WaterLines from "./WaterLines";
 import Lights from "./Lights";
+import { useEffect, useRef } from "react";
 
 export default function Experience() {
   const { backgroundColor } = useControls({
@@ -21,19 +22,44 @@ export default function Experience() {
     maxPolarAngle,
     dampingFactor,
     rotateSpeed,
+    minAzimuthAngle,
+    maxAzimuthAngle,
   } = useControls("camera controls", {
     minDistance: { value: 20, min: 0, max: 200, step: 1 },
     maxDistance: { value: 80, min: 0, max: 200, step: 1 },
-    minPolarAngle: { value: 0, min: 0, max: Math.PI, step: 0.1 },
+    minPolarAngle: { value: 0, min: 0, max: Math.PI, step: Math.PI / 12 },
     maxPolarAngle: {
-      value: Math.PI / 2 - 0.1,
+      value: Math.PI / 2 - Math.PI / 12,
       min: 0,
       max: Math.PI,
-      step: 0.1,
+      step: Math.PI / 12,
+    },
+    minAzimuthAngle: {
+      value: -Math.PI / 2,
+      min: -2 * Math.PI,
+      max: Math.PI,
+      step: Math.PI / 12,
+    },
+    maxAzimuthAngle: {
+      value: Math.PI / 2,
+      min: 0,
+      max: 2 * Math.PI,
+      step: Math.PI / 12,
     },
     dampingFactor: { value: 0.05, min: 0, max: 2, step: 0.05 },
     rotateSpeed: { value: 1, min: 0, max: 2, step: 0.05 },
   });
+
+  // @ts-expect-error: Error with OrbitControls type, but needs to be typed otherwise error when accessing uniform
+  const controlsRef = useRef<OrbitControls>(null);
+
+  useEffect(() => {
+    if (!controlsRef.current) return;
+
+    const azimuth = controlsRef.current.getAzimuthalAngle();
+    controlsRef.current.minAzimuthAngle = azimuth - maxAzimuthAngle;
+    controlsRef.current.maxAzimuthAngle = azimuth + maxAzimuthAngle;
+  }, [maxAzimuthAngle]);
 
   return (
     <>
@@ -42,6 +68,7 @@ export default function Experience() {
 
       <Lights debug={false} />
       <OrbitControls
+        ref={controlsRef}
         target={[-6, 0, 0]} // offset camera on the left
         // limit zoom in/out
         minDistance={minDistance}
@@ -49,6 +76,9 @@ export default function Experience() {
         // limit horizontal rotation
         minPolarAngle={minPolarAngle}
         maxPolarAngle={maxPolarAngle} // Math.PI / 2 = vue horizontale
+        // limit vertical rotation
+        minAzimuthAngle={minAzimuthAngle} // -45° à gauche
+        maxAzimuthAngle={maxAzimuthAngle} // +45° à droite
         // damping
         enableDamping
         dampingFactor={dampingFactor}
