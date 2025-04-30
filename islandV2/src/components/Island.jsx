bf9be7847dafa59fe09db8c75d2e687686b3d21c
Morgane Delmas <mdelmas93@gmail.com>
2025-04-30 21:47:14 +0200

    [fix] removing Leva and console log for deployment

--- islandV2/src/components/Island.jsx
+++ islandV2/src/components/Island.jsx
@@ -3,7 +3,6 @@ import { useGLTF, useTexture } from '@react-three/drei'
 export default function Island() {
   const model = useGLTF('./model/island/model.glb');
   const bakedTexture = useTexture('./model/island/bake.jpg');
-  console.log(model, bakedTexture)
   bakedTexture.flipY = false;
   
   return <mesh geometry={ model.nodes.baked.geometry } castShadow>
--- islandV2/src/index.jsx
+++ islandV2/src/index.jsx
@@ -1,17 +1,22 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
+import { Leva } from 'leva'
+
 import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas
-        camera={ {
-            fov: 50,
-            position: [ 1, 0.6, 2 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
+    <>
+        <Leva hidden={true} />
+        <Canvas
+            camera={ {
+                fov: 50,
+                position: [ 1, 0.6, 2 ]
+            } }
+            >
+            <Experience />
+        </Canvas>
+    </>
 )
