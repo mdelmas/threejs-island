21e68e025e7e12409aa8a0d87695ff9103507b1f
Morgane Delmas <mdelmas93@gmail.com>
2025-04-30 22:21:12 +0200

    [feat/wip] testing CustomShaderMaterial usage

--- islandV2/src/components/Water.jsx
+++ islandV2/src/components/Water.jsx
@@ -1,6 +1,6 @@
-// import { MeshStandardMaterial } from 'three';
-import { useGLTF, useTexture, Float } from '@react-three/drei'
-// import CustomShaderMaterial from "three-custom-shader-material";
+import { MeshStandardMaterial } from 'three';
+import { Float } from '@react-three/drei'
+import CustomShaderMaterial from "three-custom-shader-material";
 import { MeshReflectorMaterial } from '@react-three/drei';
 import { useControls } from 'leva';
 
@@ -15,7 +15,7 @@ export default function Water() {
       rotationIntensity={0}
       floatingRange={[0, 0.014]}
     >
-      <mesh rotation-x={-Math.PI / 2} receiveShadow>
+      <mesh rotation-x={-Math.PI / 2}>
         <planeGeometry args={[100, 100]} />
         <MeshReflectorMaterial
           color={waterColor}
@@ -23,14 +23,13 @@ export default function Water() {
           resolution={1024}
         />
       </mesh>
-      {/* <mesh rotation-x={-Math.PI / 2} receiveShadow>
-        <planeGeometry args={[100, 100]} />
-        <MeshReflectorMaterial
-          color="#33E4D0"
-          roughness={0}
-          resolution={1024}
+      <mesh rotation-x={-Math.PI / 2} position-y={0.001}>
+        <planeGeometry args={[1, 1]} />
+        <CustomShaderMaterial
+          color="white"
+          baseMaterial={MeshStandardMaterial}
         />
-      </mesh> */}
+      </mesh>
     </Float>
   )
 }
